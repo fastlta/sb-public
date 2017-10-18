98bf48dafa1970c4fb5138b5342fbeca60b05013
jgelhorn <jgelhorn@fast-lta.de>
2017-10-18 18:33:36 +0200

    added new coalling of object

--- monitoring/perl/sample.pl
+++ monitoring/perl/sample.pl
@@ -8,41 +8,50 @@ use FAST::ApiPublic;
 # use FAST::SBApiHelper;
 
 my $ip = '172.100.51.175';
+my $user = 'admin';
+my $pass = 'adminadmin';
 
 
-my $object = FAST::ApiPublic->new();
-
-$object->SetHost({
-        host => $ip,
-    });
-
-# print Dumper ($object->{target});
-
-$object->SetCredentials({
-        user        => 'admin',
-        password    => 'adminadmin',
-    });
+    my $headers_default = {
+                        Authorization => 'Basic '.encode_base64($user.':'.$pass),
+                         Accept        => 'application/json',
+                        }; 
 
 
-$object->SetHeaders();
+
+
+my $object = FAST::ApiPublic->new($ip, $user, $pass );
+# print Dumper ($object);
+# $object->SetHost({
+#         host => $ip,
+#     });
+
+
+# $object->SetCredentials({
+#         user        => 'admin',
+#         password    => 'adminadmin',
+#     });
+
+
+# $object->SetHeaders();
 
 
 
 # my $serials     = $object->GetBrickSerials();
 # my $uuids       = $object->GetBrickUuids();
 
-my $libs        = $object->GetLibraries();
+# my $libs        = $object->GetLibraries();
 my $vols        = $object->GetVolumes();
 
-print Dumper $vols;
+# print Dumper $vols;
 # my $lib_uuids   = $object->GetLibrary_UUIDS();
 
 # print Dumper ($object->GetBrickUUID_BySerial({serial => 'B10A0365'}));
 # print Dumper  ($uuids);
 # print Dumper ($serials);
 
-print Dumper  ($libs);
-# print Dumper  ($vols);
+# print Dumper  ($libs);
+print Dumper  ($vols);
 # print Dumper  ($lib_uuids);
 
 # print Dumper ($object->GetLibrary_UUIDS());
